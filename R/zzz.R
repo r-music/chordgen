0a9483e4fe867bf406b428ca462e331717482a99
Julio Trecenti <julio.trecenti@gmail.com>
2017-10-13 23:41:43 -0300

    globalvars and package main doc

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+utils::globalVariables(c(
+  "n_notes", "notes", "notes_c", "music21"
+))
+
+#' chordgen
+#'
+#' This is a random chord generator which uses music21 package
+#'
+#' @import music21
+#'
+#' @docType package
+#' @name chordgen
+NULL
